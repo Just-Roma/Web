d35962614cd0677cc2d5184f1aac1d0029bf2252
Roman Svetlitckii <64587275+Just-Roma@users.noreply.github.com>
2021-09-05 22:22:04 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
-onmessage = function(message) {
+
   console.log('Message received from main script');
-  var workerResult = 'Result: ' + (message.data[0] * message.data[1]);
-  console.log('Posting message back to main script');
-  postMessage(workerResult);
-}
+
+
