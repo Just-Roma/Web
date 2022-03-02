1c979b6e55c3dc1404b183919abfa0145c7800ec
Roman Svetlitckii <64587275+Just-Roma@users.noreply.github.com>
2022-03-02 20:27:25 +0100

    Update FractalFlame.js

--- Just-Something-Pretty/Fractal-Flame/FractalFlame.js
+++ Just-Something-Pretty/Fractal-Flame/FractalFlame.js
@@ -7,78 +7,81 @@
 
 /* Each function has a set of parameters - coefficients of an affine combination.
 
-	(Xn+1) = (a b) * (Xn) + (c)
-	(Yn+1)   (d e)   (Yn)   (f)
-	
-	These parameters will be randomly chosen each time a new Fi is produced.
-	
-	According to the original paper the affine transformation is better to be contractive, which means that the following conditions must be checked:
-	
-	1) a^2 + d^2 < 1
-	2) b^2 + e^2 < 1
-	3) a^2 + b^2 + d^2 + e^2 < 1 + (ae - bd)^2
-	
-	They will be chosen from the set [-1;1), 1 is not included because of the way Math.random() works.
-	Some of these combinations will probably result in boring/ugly results.
+   (Xn+1) = (a b) * (Xn) + (c)
+   (Yn+1)   (d e)   (Yn)   (f)
+
+   These parameters will be randomly chosen each time a new Fi is produced.
+
+   According to the original paper the affine transformation is better to be contractive, which means that the following conditions must be checked:
+
+   1) a^2 + d^2 < 1
+   2) b^2 + e^2 < 1
+   3) a^2 + b^2 + d^2 + e^2 < 1 + (ae - bd)^2
+
+   They will be chosen from the set [-1;1), 1 is not included because of the way Math.random() works.
+   Some of these combinations will probably result in boring/ugly results.
 */
 
 function assignCoeffs(size){
-	let out1 = [];
-	let out2 = [];
-	let a,b,c,d,e,f,rgb;
+  let out1 = [];
+  let out2 = [];
+  let a,b,c,d,e,f,rgb;
 
-	while(size > 0){
-		do{
-			do{
-				a = Math.random()*2 - 1;
-				d = Math.random()*2 - 1;
-			}while((a**2+d**2) >= 1);
-			do{
-				b = Math.random()*2 - 1;
-				e = Math.random()*2 - 1;
-			}while((b**2 + e**2) >= 1);
-		}while((a**2 + d**2 + b**2 + e**2) >= (1+(a*e - b*d)**2));
-		c = Math.random()*2 - 1;
-		f = Math.random()*2 - 1;
-		out1.push([a,b,c,d,e,f]);
+  while(size > 0){
+    do{
+      do{
+        a = Math.random()*2 - 1;
+        d = Math.random()*2 - 1;
+      }while((a**2+d**2) >= 1);
 
-	do{
-		rgb = [Math.round(Math.random()*255), Math.round(Math.random()*155), Math.round(Math.random()*155)];
-	}while(Math.max(...rgb) < 100);
+      do{
+        b = Math.random()*2 - 1;
+        e = Math.random()*2 - 1;
+      }while((b**2 + e**2) >= 1);
 
-		out2.push(rgb);
-		size--;
-	}
-	return [out1,out2];
+    }while((a**2 + d**2 + b**2 + e**2) >= (1+(a*e - b*d)**2));
+
+    c = Math.random()*2 - 1;
+    f = Math.random()*2 - 1;
+    out1.push([a,b,c,d,e,f]);
+
+    do{
+      rgb = [Math.round(Math.random()*255), Math.round(Math.random()*155), Math.round(Math.random()*155)];
+    }while(Math.max(...rgb) < 100);
+
+    out2.push(rgb);
+    size--;
+  }
+  return [out1,out2];
 }
 
 
 function workIt(event){
-	if (typeof event.data == 'boolean'){
-		common.workerDone = true;
-	}
-	else{
-		common.ctx.putImageData(event.data, 0, 0); // print the image on the canvas.
-	}
+  if (typeof event.data == 'boolean'){
+    common.workerDone = true;
+  }
+  else{
+    common.ctx.putImageData(event.data, 0, 0); // print the image on the canvas.
+  }
 
-	// This little part "turns on" the "Create" button if the image was already printed and a user is not in the info menu.
-	if(common.disabled && common.allowed){
-		document.getElementById('ButtonCreate').disabled='';
-		common.disabled = false;
-		document.getElementById('ButtonCreate').style.cursor = 'pointer';
-	}
+  // This little part "turns on" the "Create" button if the image was already printed and a user is not in the info menu.
+  if(common.disabled && common.allowed){
+    document.getElementById('ButtonCreate').disabled='';
+    common.disabled = false;
+    document.getElementById('ButtonCreate').style.cursor = 'pointer';
+  }
 }
 
 // This function closes the old worker and creates a new one.
 // It also sends a message to the Worker, which includes the sizes of the matrix, affine coeefs and mode value.
 function modifyWorker(){
-	if (!common.workerDone){
-		common.worker.terminate();
-		common.worker = new Worker(URL.createObjectURL(common.blob));
-	}
-	common.workerDone = false;
-	common.worker.postMessage([common.width, common.height, assignCoeffs(common.numberOfCoeffs), document.getElementById('selectFunc').value]);
-	common.worker.onmessage = workIt;
+  if (!common.workerDone){
+    common.worker.terminate();
+    common.worker = new Worker(URL.createObjectURL(common.blob));
+  }
+  common.workerDone = false;
+  common.worker.postMessage([common.width, common.height, assignCoeffs(common.numberOfCoeffs), document.getElementById('selectFunc').value]);
+  common.worker.onmessage = workIt;
 }
 
 
