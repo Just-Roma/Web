44b795261bf8108fbd4ab51992ebf6ec2068d550
Roman Svetlitckii <64587275+Just-Roma@users.noreply.github.com>
2022-03-25 17:21:30 +0100

    Update EventListeners.js

--- Just-Something-Pretty/Fractal-Flame/EventListeners.js
+++ Just-Something-Pretty/Fractal-Flame/EventListeners.js
@@ -1,20 +1,34 @@
 "use strict";
 
 /* References to the html elements */
-let InfoPageNode = document.getElementById('infoPage');
-let DependentParametersNode = document.getElementById('ExtraParametersDiv');
-let BlobDivNode = document.getElementById('Blob_Div');
-let PDJDivNode = document.getElementById('PDJ_Div');
-let JuliaNDivNode = document.getElementById('JuliaN_Div');
+let InfoPageNode = document.getElementById('InfoPage');
+let ExtraParametersNode = document.getElementById('ExtraParametersDiv');
+
+// Functions' div blocks, which hold the corresponding html elements associated with functions' parameters.
+let Fan2DivNode = document.getElementById('Fan2Div');
+let Rings2DivNode = document.getElementById('Rings2Div');
+let BlobDivNode = document.getElementById('BlobDiv');
+let PDJDivNode = document.getElementById('PDJDiv');
+let JuliaNDivNode = document.getElementById('JuliaNDiv');
+
+// Functions' parameters nodes(selects, textareas...).
 let ButtonCreateNode = document.getElementById('ButtonCreate');
 let ButtonInfoNode = document.getElementById('ButtonInfo');
-let BlobNode = document.getElementById('BlobPar');
-let PDJNode1 = document.getElementById('PDJ_Select_p1');
-let PDJNode2 = document.getElementById('PDJ_Select_p2');
-let PDJNode3 = document.getElementById('PDJ_Select_p3');
-let PDJNode4 = document.getElementById('PDJ_Select_p4');
-let JuliaNPower = document.getElementById('JuliaNPower');
-let JuliaNDistance = document.getElementById('JuliaNDistance');
+
+let Fan2XParNode = document.getElementById('Fan2X');
+let Fan2YParNode = document.getElementById('Fan2Y');
+
+let Rings2ParNode = document.getElementById('Rings2val');
+
+let BlobParNode = document.getElementById('BlobPar');
+
+let PDJNodePar1 = document.getElementById('PDJ_Select_p1');
+let PDJNodePar2 = document.getElementById('PDJ_Select_p2');
+let PDJNodePar3 = document.getElementById('PDJ_Select_p3');
+let PDJNodePar4 = document.getElementById('PDJ_Select_p4');
+
+let JuliaNPowerParNode = document.getElementById('JuliaNPower');
+let JuliaNDistanceParNode = document.getElementById('JuliaNDistance');
 
 
 window.addEventListener('resize', 
@@ -53,13 +67,30 @@ FuncNode.addEventListener('change',
     // This part adds/hides the block with dependent parameters
     switch (FuncNode.value){
 
+      case 'Fan2':
+        common.refToExtraBlock.display = 'none';
+        common.refToExtraBlock = Fan2DivNode.style;
+        common.refToExtraBlock.display = 'block';
+        ExtraParametersNode.style.display = 'block';
+        common.extraPars.p1 = Number(Fan2XParNode.value);
+        common.extraPars.p2 = Number(Fan2YParNode.value);
+        break;
+
+      case 'Rings2':
+        common.refToExtraBlock.display = 'none';
+        common.refToExtraBlock = Rings2DivNode.style;
+        common.refToExtraBlock.display = 'block';
+        ExtraParametersNode.style.display = 'block';
+        common.extraPars.p1 = Number(Rings2ParNode.value);
+        break;
+
       case 'Blob':
         common.refToExtraBlock.display = 'none';
         common.refToExtraBlock = BlobDivNode.style;
         common.refToExtraBlock.display = 'block';
-        DependentParametersNode.style.display = 'block';
+        ExtraParametersNode.style.display = 'block';
         let randomDefault = 2+Math.round(10*Math.random());
-        BlobNode.value = randomDefault;
+        BlobParNode.value = randomDefault;
         common.extraPars.p3 = randomDefault;
         break;
 
@@ -67,27 +98,26 @@ FuncNode.addEventListener('change',
         common.refToExtraBlock.display = 'none';
         common.refToExtraBlock = PDJDivNode.style;
         common.refToExtraBlock.display = 'block';
-        DependentParametersNode.style.display = 'block';
-        common.extraPars.p1 = Number(PDJNode1.value);
-        common.extraPars.p2 = Number(PDJNode2.value);
-        common.extraPars.p3 = Number(PDJNode3.value);
-        common.extraPars.p4 = Number(PDJNode4.value);
+        ExtraParametersNode.style.display = 'block';
+        common.extraPars.p1 = Number(PDJNodePar1.value);
+        common.extraPars.p2 = Number(PDJNodePar2.value);
+        common.extraPars.p3 = Number(PDJNodePar3.value);
+        common.extraPars.p4 = Number(PDJNodePar4.value);
         break;
 
       case 'JuliaN':
         common.refToExtraBlock.display = 'none';
         common.refToExtraBlock = JuliaNDivNode.style;
         common.refToExtraBlock.display = 'block';
-        DependentParametersNode.style.display = 'block';
-        common.extraPars.p1 = Number(JuliaNPower.value);
-        common.extraPars.p2 = 2;
-        JuliaNDistance.value = 2;
+        ExtraParametersNode.style.display = 'block';
+        common.extraPars.p1 = Number(JuliaNPowerParNode.value);
+        common.extraPars.p2 = Number(JuliaNDistanceParNode.value);
         break;
 
       default:
         common.refToExtraBlock.display = 'none';
         common.refToExtraBlock = {'display': null};
-        DependentParametersNode.style.display = 'none';
+        ExtraParametersNode.style.display = 'none';
     }
 });
 
@@ -98,55 +128,108 @@ AffineNode.addEventListener('change',
 });
 
 
-BlobNode.addEventListener('change',
+/* ---------------------------------------------------------------------------------------- */
+/* The event listeners for the blocks(divs) of functions' extra parameters are defined here */
+
+
+// Fan2's x and y parameters.
+Fan2XParNode.addEventListener('change',
   () => {
-    let UserInput = Math.abs(Number(BlobNode.value));
+    let UserInput = Math.abs(Number(Fan2XParNode.value));
+    if(UserInput && UserInput <= 0.5 && UserInput >= 0.1){
+      Fan2XParNode.value = UserInput;
+      common.extraPars.p1 = UserInput;
+    }else{
+      alert("Please enter a number from 0.1 to 0.5.");
+      Fan2XParNode.value = common.extraPars.p1;
+    }
+});
+Fan2YParNode.addEventListener('change',
+  () => {
+    let UserInput = Math.abs(Number(Fan2YParNode.value));
+    if(UserInput && UserInput <= 0.5 && UserInput >= 0.1){
+      Fan2YParNode.value = UserInput;
+      common.extraPars.p2 = UserInput;
+    }else{
+      alert("Please enter a number from 0.1 to 0.5.");
+      Fan2YParNode.value = common.extraPars.p2;
+    }
+});
+
+
+// Rings2's parameter.
+Rings2ParNode.addEventListener('change',
+  () => {
+    let UserInput = Math.abs(Number(Rings2ParNode.value));
+    if(UserInput && UserInput <= 1 && UserInput >= 0.2){
+      Rings2ParNode.value = UserInput;
+      common.extraPars.p1 = UserInput;
+    }else{
+      alert("Please enter a number from 0.2 to 1.");
+      Rings2ParNode.value = common.extraPars.p1;
+    }
+});
+
+// Blob's waves(p3) parameter. p1=Blob.high and p2=Blob.low are set to constants 1.5 and 0.5, since they seem to affect only the size of images.
+BlobParNode.addEventListener('change',
+  () => {
+    let UserInput = Math.abs(Number(BlobParNode.value));
     if(UserInput && UserInput <= 1000){
-      BlobNode.value = Math.ceil(UserInput);
+      BlobParNode.value = Math.ceil(UserInput);
       common.extraPars.p3 = Math.ceil(UserInput);
     }else{
       alert("Please enter a number from 1 to 1000. It will be rounded up.");
-      BlobNode.value = common.extraPars.p3;
+      BlobParNode.value = common.extraPars.p3;
     }
 });
 
-
-PDJNode1.addEventListener('change',
+// PDJ's 4 parameters
+PDJNodePar1.addEventListener('change',
   () => {
-    common.extraPars.p1 = Number(PDJNode1.value);
+    common.extraPars.p1 = Number(PDJNodePar1.value);
+});
+PDJNodePar2.addEventListener('change',
+  () => {
+    common.extraPars.p2 = Number(PDJNodePar2.value);
+});
+PDJNodePar3.addEventListener('change',
+  () => {
+    common.extraPars.p3 = Number(PDJNodePar3.value);
+});
+PDJNodePar4.addEventListener('change',
+  () => {
+    common.extraPars.p4 = Number(PDJNodePar4.value);
 });
 
-PDJNode2.addEventListener('change',
-  () => {
-    common.extraPars.p2 = Number(PDJNode2.value);
-});
 
-PDJNode3.addEventListener('change',
+// JuliaN's power and distance.
+JuliaNPowerParNode.addEventListener('change',
   () => {
-    common.extraPars.p3 = Number(PDJNode3.value);
+    common.extraPars.p1 = Number(JuliaNPowerParNode.value);
 });
-
-PDJNode4.addEventListener('change',
+JuliaNDistanceParNode.addEventListener('change',
   () => {
-    common.extraPars.p4 = Number(PDJNode4.value);
-});
+    let UserInput = Number(JuliaNDistanceParNode.value);
 
-JuliaNPower.addEventListener('change',
-  () => {
-    common.extraPars.p2 = Number(JuliaNPower.value);
-});
-
-JuliaNDistance.addEventListener('change',
-  () => {
-    let UserInput = Math.abs(Number(JuliaNDistance.value));
-    if(UserInput && UserInput <= 5 && UserInput >= 1){
-      JuliaNDistance.value = UserInput;
-      common.extraPars.p1 = UserInput;
+    if(UserInput && UserInput <= 10 && UserInput >= -10){
+      if(UserInput < 1 && UserInput > -1){UserInput = 1;}
+      JuliaNDistanceParNode.value = UserInput;
+      common.extraPars.p2 = UserInput;
     }else{
-      alert("Please enter a number from 1 to 5.");
-      JuliaNDistance.value = common.extraPars.p1;
+      if(UserInput === 0){
+        JuliaNDistanceParNode.value = 1;
+        common.extraPars.p2 = 1;
+      }else{
+        alert("Please enter a number from -10 to 10. The range (-1,1) will be converted to 1.");
+        JuliaNDistanceParNode.value = common.extraPars.p2;
+      }
     }
 });
+/*__________________________________________________________________________________*/
+
+
+/* -------------------------------------------------------------------------------- */
+/*         Event listeners for the main buttons("Create" and "Info")                */
 
 
 /* There is a chance that the first frame will need several seconds to be printed. 
@@ -161,6 +244,9 @@ ButtonCreateNode.addEventListener('click',
   }
 );
 
+/* When the "Info" button is clicked the "Create" button will be disabled.
+   It will be reactivated after a user closes the Info menu.
+*/
 ButtonInfoNode.addEventListener('click', 
   () => {
   if(common.allowed){
